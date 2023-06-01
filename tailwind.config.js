ee1d9978ecaf0901c804f5de9f89b9bbc971b59d
Alistair <alistairm2022@gmail.com>
2023-06-01 08:56:41 +0530

    Working frontend

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,63 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      backgroundColor: {
+        'dark-blue': 'rgb(0, 0, 15)'
+      },
+      boxShadow: {
+        'custom-yellow': 'rgb(250, 204, 21) 0px 22px 70px 4px',
+        'custom-indigo': 'rgb(129, 140, 248) 0px 22px 70px 4px',
+        'custom-orange': 'rgb(251, 146, 60) 0px 22px 70px 4px'
+      },
+      keyframes: {
+        colorSwitch: {
+          '0%': { boxShadow: 'rgb(250, 204, 21) 0px 0px 70px 10px' },
+          '50%': { boxShadow: 'rgb(129, 140, 248) 0px 0px 70px 10px' },
+          '100%': { boxShadow: 'rgb(251, 146, 60) 0px 0px 70px 10px' }
+        },
+        colorSwitchText: {
+          '0%': { color: 'rgb(250, 204, 21)' },
+          '50%': { color: 'rgb(129, 140, 248)' },
+          '100%': { color: 'rgb(251, 146, 60)' }
+        },
+        colorSwitchButton: {
+          '0%': {
+            border: '4px solid rgb(250, 204, 21)',
+            color: 'rgb(253, 224, 71)',
+            boxShadow: 'rgb(250, 204, 21) 0px 0px 70px 10px'
+          },
+          '50%': {
+            border: '4px solid rgb(129, 140, 248)',
+            color: 'rgb(129, 140, 248)',
+            boxShadow: 'rgb(129, 140, 248) 0px 0px 70px 10px'
+          },
+          '100%': {
+            border: '4px solid rgb(251, 146, 60)',
+            color: 'rgb(251, 146, 60)',
+            boxShadow: 'rgb(251, 146, 60) 0px 0px 70px 10px'
+          }
+        },
+        colorSwitchButtonHover: {
+          '0%': {
+            border: '4px solid rgb(250, 204, 21)',
+            backgroundColor: 'rgb(250, 204, 21)',
+          },
+          '50%': {
+            border: '4px solid rgb(129, 140, 248)',
+            backgroundColor: 'rgb(129, 140, 248)',
+          },
+          '100%': {
+            border: '4px solid rgb(251, 146, 60)',
+            backgroundColor: 'rgb(251, 146, 60)',
+          }
+        }
+      },
+      animation: {
+        colorSwitch: 'colorSwitch 4s linear infinite alternate',
+        colorSwitchText: 'colorSwitchText 4s linear infinite alternate',
+        colorSwitchButton: 'colorSwitchButton 4s linear infinite alternate',
+        colorSwitchButtonHover: 'colorSwitchButtonHover 4s linear infinite alternate'
+      }
     },
   },
   plugins: [],
